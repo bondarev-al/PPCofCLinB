773477984e8ff7f5c9b2c46120ce31c258250f4a
Alexander Bondarev <bondarev-al@mail.ru>
2021-02-24 02:19:14 +0300

    Исправлена опечатка

--- editbuildingwindow.cpp
+++ editbuildingwindow.cpp
@@ -87,7 +87,7 @@ void EditBuildingWindow::on_floor_but_clicked()
 void EditBuildingWindow::on_sizeAct_triggered()
 {
     int height = QInputDialog::getInt(nullptr, "Ввод количества строк", "Количество строк:", height_floor, 1, 10);
-    int width = QInputDialog::getInt(nullptr, "Ввод количества столбцов", "Количество стобцов:", width_floor, 1, 15);
+    int width = QInputDialog::getInt(nullptr, "Ввод количества столбцов", "Количество столбцов:", width_floor, 1, 15);
     saveFloorWalls();
     setSize(height, width);
     showFloor(floor_number);
